c6c58f467ef5f1b17f2cd76cbe4ca400f25c0f9a
Luciano Duarte <lucianod98@gmail.com>
2024-09-10 20:54:22 -0300

    aula 1

---
+++ aula1/index.js
@@ -0,0 +1,41 @@
+// // salve salve
+// let mensagem = "bahia 3"
+// console.log(mensagem);
+
+// // arrays, objetos
+// let metas = ["jooj","chef"]
+// console.log(metas[1] + metas[0])
+
+// let meta = {
+//     value: "tomar uma pinga por mês",
+//     adress: 2,
+//     checked: false, 
+//     log: (info) => {
+//         console.log(info)
+//     }
+
+// }
+
+// meta.value = ("agora é cachaça!")
+// meta.log(meta.value)
+
+// // function // arrow function
+// const criarMeta = () => {}
+
+// // function criarMeta = () {}
+
+
+let meta = {
+    value: "boraaaa billll!",
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "boraaaa caraio!",
+        checked: false,
+    }
+]
+    
+console.log(meta[1].value)
